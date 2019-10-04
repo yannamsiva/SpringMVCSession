45fd10381842a9b49152799897c007c712620cfd
Admin <Admin@Admin-PC>
2019-10-04 20:24:42 +0530

    welcome boss

---
+++ Shiva_basic/src/com/shiva/FirstServlet.java
@@ -0,0 +1,71 @@
+package com.shiva;
+
+import java.io.IOException;
+
+import javax.jws.soap.InitParam;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet(urlPatterns={"/first","/second"}
+,loadOnStartup=1,
+asyncSupported=true,
+initParams={
+		@WebInitParam(name="uname",value="shiva")
+}
+
+
+		)
+
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+       // super();
+        // TODO Auto-generated constructor stub
+        System.out.println("Welcome to Shiva");
+    }
+
+   
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	System.out.println(" Generic Servlet Welcome to service");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to service");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to doPost");
+	}
+
+}
